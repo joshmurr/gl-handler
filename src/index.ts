1376a3d13a4f8f9c3a50e52f49fb36111094865e
Josh Murr <joshuadavidmurr@gmail.com>
2022-06-20 19:09:00 +0100

    Webgl constants by num

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import GL_Handler from './gl-handler'
 import Geometry from './geometry/Geometry'
 import Quad from './geometry/Quad'
+import { constants } from './constants'
 import * as Types from './types'
 
-export { GL_Handler, Geometry, Quad, Types }
+export { GL_Handler, Geometry, Quad, Types, constants }
