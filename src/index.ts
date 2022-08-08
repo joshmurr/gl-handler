f739350c26d16323a5ef4cf913c34f9e06be328f
Josh Murr <joshuadavidmurr@gmail.com>
2022-08-08 19:46:37 +0100

    Arcball

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import GL_Handler from './gl-handler'
 import Geometry from './geometry/Geometry'
 import Quad from './geometry/Quad'
 import ShaderToy from './geometry/ShaderToy'
+import Arcball from './arcball'
 import { constants } from './constants'
 import * as Types from './types'
 
-export { GL_Handler, Geometry, Quad, ShaderToy, Types, constants }
+export { GL_Handler, Geometry, Quad, ShaderToy, Arcball, Types, constants }
