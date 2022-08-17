c5855780e3087e9530468eadd0f17a8920d5b7b8
Josh Murr <joshuadavidmurr@gmail.com>
2022-08-17 13:02:11 +0100

    Export camera

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import GL_Handler from './gl-handler'
 import Geometry from './geometry/Geometry'
+import Camera from './camera'
 import Quad from './geometry/Quad'
 import ShaderToy from './geometry/ShaderToy'
 import Arcball from './arcball'
 import { constants } from './constants'
 import * as Types from './types'
 
-export { GL_Handler, Geometry, Quad, ShaderToy, Arcball, Types, constants }
+export { GL_Handler, Geometry, Camera, Quad, ShaderToy, Arcball, Types, constants }
